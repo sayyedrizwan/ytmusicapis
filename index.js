d1878b0e480dc793aaa84d755ba9fdc476ae7dd0
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2021-04-01 17:33:34 +0530

    updated new apis

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const YoutubeMusicApi = require('youtube-music-api')
+const { listCharts, getChart } = require('billboard-top-100');
+var moment = require('moment-timezone');
 
 const api = new YoutubeMusicApi()
 
@@ -20,10 +22,32 @@ app.get('/artists', (req, res) => {
     })
 })
 
-app.get('/artistslist', (req, res) => {
-    api.initalize().then(info => {
-        api.getArtist("UCGvj8kfUV5Q6lzECIrGY19g").then(result => res.send(result))
-    })
+app.get('/getartists', (req, res) => {
+    getChart('artist-100',moment().tz("America/Los_Angeles").format("YYYY-MM-DD"), (err, chart) => {
+        if (err) console.log(err);
+        res.send(chart)
+    });
+})
+
+app.get('/topalbums', (req, res) => {
+    getChart('top-album-sales',moment().tz("America/Los_Angeles").format("YYYY-MM-DD"), (err, chart) => {
+        if (err) console.log(err);
+        res.send(chart)
+    });
+})
+
+app.get('/topsongs', (req, res) => {
+    getChart('hot-100',moment().tz("America/Los_Angeles").format("YYYY-MM-DD"), (err, chart) => {
+        if (err) console.log(err);
+        res.send(chart)
+    });
+})
+
+app.get('/lists', (req, res) => {
+    listCharts((err, charts) => {
+        if (err) console.log(err);
+        res.send(charts)
+    });
 })
 
 app.listen(port, () => {
